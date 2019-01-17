1ce370d26ae335645437034b33e5aae9d245acbf
wood101 <puuta_13@hotmail.com>
2019-01-17 14:32:13 +0200

    index.js changes

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const { Client } = require('pg');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static('build'))
+
+app.use(morgan(':method :url :json :status :response-time ms'))
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
 
-client.connect();
+app.get('/api/ohjaajat', (request, response) => {
+    client.connect()
 
-client.query('SELECT * FROM ohjaaja;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+    client.query('SELECT * FROM ohjaaja;', (err, res) => {
+    if (err) throw err
+    response.json(res.rows.map(JSON.stringify(row)))
+    client.end()
+    })
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

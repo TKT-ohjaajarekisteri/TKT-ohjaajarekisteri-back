67e3819b7dd2832b2c778f45aef082dee3565718
wood101 <puuta_13@hotmail.com>
2019-01-31 16:48:25 +0200

    config

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,15 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-
 let port = null
 let databaseUrl = null
 let logging = true
+
+if (process.env.NODE_ENV === 'production') {
+  port = process.env.PORT
+  databaseUrl = process.env.DATABASE_URL
+  logging = false
+}
 
 if (process.env.NODE_ENV === 'test') {
   port = process.env.TEST_PORT

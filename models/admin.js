430ca62268edd05b4096e7114aeedab676785eec
wood101 <puuta_13@hotmail.com>
2019-02-04 11:12:53 +0200

    sequelize models

---
+++ models/admin.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//Sequelize model for Admin database object
+module.exports = (sequelize, Sequelize) => {
+  const Admin = sequelize.define('Admin', {
+    admin_id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+      unique: true
+    },
+    username: {
+      type: Sequelize.STRING(55),
+      allowNull: false
+    },
+    password: {
+      type: Sequelize.STRING(55),
+      allowNull: false
+    }
+  })
+
+  return Admin
+}

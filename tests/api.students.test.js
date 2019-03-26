9f4f2e0760646dae30b8c47fa18c5365b3e36f3c
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-03-26 16:34:58 +0200

    fixed one test, added test db migrate undo

--- tests/api.students.test.js
+++ tests/api.students.test.js
@@ -156,12 +156,12 @@ describe('tests for the students controller', () => {
         describe('update and delete tests', () => {
           test('Student can update his/her own data with PUT /api/students/:id', async () => {
             const studentBefore = await db.Student.findOne({ where: { student_id: students[index].student_id } })
-            expect(studentBefore.teachesInEnglish).toBeTruthy()
+            expect(studentBefore.no_english).toBeFalsy()
 
             await api
               .put(`/api/students/${users[index].user_id}`)
               .set('Authorization', `bearer ${studentToken}`)
-              .send({ email: 'maili@hotmail.com', phone: '0402356543', experience: 'very good', teachesInEnglish: 'false'})
+              .send({ email: 'maili@hotmail.com', phone: '0402356543', experience: 'very good', no_english: 'true'})
               .expect(201)
       
             const updatedStudent = await db.Student.findOne({ where: { student_id: students[index].student_id } })
